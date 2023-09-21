7629fd62e78d7b89d78b3590fed5848ec1d7edf9
kardelenkeles <kar_di44@hotmail.com>
2023-09-21 11:02:09 +0300

    subscription added

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,177 +1,173 @@
-import {ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {Task} from '../model/task';
 import {
-    CdkDragDrop,
-    moveItemInArray,
-    transferArrayItem,
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import {Select, Store} from '@ngxs/store';
 import {
-    AddTask,
-    DeleteTask,
-    GetAllTasks,
-    UpdateTask,
+  AddTask,
+  DeleteTask,
+  GetAllTasks,
+  UpdateTask,
 } from '../state/action/task.action';
 import {TaskSelector} from '../state/selector/task.selector';
 import {Observable} from 'rxjs';
+import {TaskService} from "../service/task.service";
 
 @Component({
-    selector: 'app-kanban',
-    templateUrl: './kanban.component.html',
-    styleUrls: ['./kanban.component.css'],
+  selector: 'app-kanban',
+  templateUrl: './kanban.component.html',
+  styleUrls: ['./kanban.component.css'],
 })
 export class KanbanComponent implements OnInit {
-    @Select(TaskSelector.items)
-    allTasks$: Observable<Task[]>;
+  @Select(TaskSelector.items)
+  allTasks$: Observable<Task[]>;
 
-    editedItemId: number | null = null;
-    editedItem: number | null = null;
+  editedItemId: number | null = null;
+  editedItem: number | null = null;
 
-    allTasks: any = {
+  allTasks: any = {
+    backlog: [],
+    todo: [],
+    inProgress: [],
+    done: [],
+  };
+
+  newId: number;
+  newHeader: string;
+  newContent: string;
+  newLabel: string = "";
+
+  columns = ['backlog', 'todo', 'inProgress', 'done'];
+
+  constructor(private store: Store,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(GetAllTasks);
+
+    this.allTasks$.subscribe((data) => {
+      this.allTasks = {
         backlog: [],
         todo: [],
         inProgress: [],
         done: [],
-    };
-
-    newId: number;
-    newHeader: string;
-    newContent: string;
-    newLabel: string = "";
-
-    columns = ['backlog', 'todo', 'inProgress', 'done'];
-
-    constructor(private store: Store,
-    ) {
-
-    }
-
-
-
-    ngOnInit(): void {
-        this.store.dispatch(GetAllTasks);
-
-        this.allTasks$.subscribe((data) => {
-            this.allTasks = {
-                backlog: [],
-                todo: [],
-                inProgress: [],
-                done: [],
-            };
-            data.forEach((item) => {
-                if (!this.allTasks[item.status]) {
-                    this.allTasks[item.status] = [];
-                }
-                return this.allTasks[item.status].push(item);
-            });
-            this.columns = Object.keys(this.allTasks);
-        });
-
-    }
-
-
-    addTask() {
-        this.store.dispatch(
-            new AddTask({
-                header: this.newHeader,
-                content: this.newContent,
-                id: this.newId,
-                label: this.newLabel,
-                status: 'backlog',
-            }),
-        );
-        this.newHeader = '';
-        this.newContent = '';
-        this.newLabel = '';
-    }
-
-    updateTask(id: number, item: any) {
-        this.store.dispatch(new UpdateTask(id, {
-            header: this.newHeader,
-            content: this.newContent
-        }));
-        this.newHeader = '';
-        this.newContent = '';
-
-    }
-
-    updateLabel(id: number, item: any) {
-        const updatedLabel = `${item.label}, ${this.newLabel}`.trim();
-
-        this.store.dispatch(new UpdateTask(id, {
-            label: updatedLabel
-        }))
-        this.newLabel = '';
-        this.store.dispatch(GetAllTasks);
-
-    }
-
-
-    deleteLabel(item: any, labelToDelete: string) {
-
-        const updatedLabels = item.label.split(',').map((label: string) => label.trim()).filter((label: string) => label !== labelToDelete);
-
-        const updatedLabel = updatedLabels.join(', ');
-
-        this.store.dispatch(new UpdateTask(item.id, {
-            label: updatedLabel
-        }))
-        this.store.dispatch(GetAllTasks);
-
-    }
-
-    parseLabels(value: string) {
-        return value?.split(',').map((part) => part.trim());
-    }
-
-    startEditing(id: number) {
-        if (this.editedItemId === id) {
-            this.cancelEdit();
-        } else {
-            this.editedItemId = id;
+      };
+      data.forEach((item) => {
+        if (!this.allTasks[item.status]) {
+          this.allTasks[item.status] = [];
         }
-    }
+        return this.allTasks[item.status].push(item);
+      });
+      this.columns = Object.keys(this.allTasks);
+    });
 
-    openLabelInput(id: number) {
-        if (this.editedItem === id) {
-            this.cancelEdit();
-        } else {
-            this.editedItem = id;
-        }
-    }
-
-    cancelEdit() {
-        this.editedItemId = null;
-        this.store.dispatch(GetAllTasks);
-    }
-
-    deleteTask(id: number) {
-        if (confirm('Do you want to delete this task?')) {
-            this.store.dispatch(new DeleteTask(id));
-        }
-    }
+  }
 
 
-    drop(event: CdkDragDrop<Task[]>, taskStatus: string) {
-        const item = event.previousContainer.data[event.previousIndex];
-        if (event.previousContainer === event.container) {
-            moveItemInArray(
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex,
-            );
-        } else {
-            transferArrayItem(
-                event.previousContainer.data,
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex,
-            );
-            this.updateTask(item.id, {
-                status: taskStatus,
-            });
-        }
+  addTask() {
+    this.store.dispatch(
+      new AddTask({
+        header: this.newHeader,
+        content: this.newContent,
+        id: this.newId,
+        label: this.newLabel,
+        status: 'backlog',
+      }),
+    );
+    this.newHeader = '';
+    this.newContent = '';
+    this.newLabel = '';
+  }
+
+  updateTask(id: number, item: any) {
+    this.store.dispatch(new UpdateTask(id, {
+      header: this.newHeader,
+      content: this.newContent
+    }));
+    this.newHeader = '';
+    this.newContent = '';
+
+  }
+
+  updateLabel(id: number, item: any) {
+    const updatedLabel = `${item.label}, ${this.newLabel}`.trim();
+    this.store.dispatch(new UpdateTask(id, {
+      label: updatedLabel
+    }))
+      .subscribe(() => {
+        this.store.dispatch(new GetAllTasks());
+      });
+    this.newLabel = '';
+  }
+
+
+  deleteLabel(item: any, labelToDelete: string) {
+    const updatedLabels = item.label.split(',').map((label: string) => label.trim()).filter((label: string) => label !== labelToDelete);
+    const updatedLabel = updatedLabels.join(', ');
+    this.store.dispatch(new UpdateTask(item.id, {
+      label: updatedLabel
+    }))
+      .subscribe(() => {
+        this.store.dispatch(new GetAllTasks());
+      });
+  }
+
+  parseLabels(value: string) {
+    return value?.split(',').map((part) => part.trim());
+  }
+
+  startEditing(id: number) {
+    if (this.editedItemId === id) {
+      this.cancelEdit();
+    } else {
+      this.editedItemId = id;
     }
+  }
+
+  openLabelInput(id: number) {
+    if (this.editedItem === id) {
+      this.cancelEdit();
+    } else {
+      this.editedItem = id;
+    }
+  }
+
+  cancelEdit() {
+    this.editedItemId = null;
+    this.store.dispatch(GetAllTasks);
+  }
+
+  deleteTask(id: number) {
+    if (confirm('Do you want to delete this task?')) {
+      this.store.dispatch(new DeleteTask(id));
+    }
+  }
+
+
+  drop(event: CdkDragDrop<Task[]>, taskStatus: string) {
+    const item = event.previousContainer.data[event.previousIndex];
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      this.updateTask(item.id, {
+        status: taskStatus,
+      });
+    }
+  }
 
 
 }
--- src/app/state/state/board.state.ts
+++ src/app/state/state/board.state.ts
@@ -1,87 +1,76 @@
-import { Injectable } from '@angular/core';
-import { Action, Selector, State, StateContext } from '@ngxs/store';
-import { BoardStateModel } from '../../model/boardStateModel';
+import {Injectable} from '@angular/core';
+import {Action, Selector, State, StateContext} from '@ngxs/store';
+import {BoardStateModel} from '../../model/boardStateModel';
 import {
-  AddBoard,
-  DeleteBoard,
-  GetAllBoards,
-  UpdateBoard,
+    AddBoard,
+    DeleteBoard,
+    GetAllBoards,
+    UpdateBoard,
 } from '../action/board.action';
-import { Board } from '../../model/board';
-import { v4 as uuidv4 } from 'uuid';
-import { tap } from 'rxjs';
-import { BoardService } from '../../service/board.service';
-import { patch, removeItem } from '@ngxs/store/operators';
+import {Board} from '../../model/board';
+import {v4 as uuidv4} from 'uuid';
+import {tap} from 'rxjs';
+import {BoardService} from '../../service/board.service';
+import {patch, removeItem} from '@ngxs/store/operators';
 
 @State<BoardStateModel>({
-  name: 'board',
-  defaults: {
-    items: [],
-  },
+    name: 'board',
+    defaults: {
+        items: [],
+    },
 })
 @Injectable()
 export class BoardState {
-  constructor(private boardService: BoardService) {}
+    constructor(private boardService: BoardService) {
+    }
 
-  @Action(AddBoard)
-  // addBoard(ctx: StateContext<BoardStateModel>, action: AddBoard) {
-  //     const state = ctx.getState();
-  //     const newItem: Board = {
-  //         id: action.id,
-  //         name: action.name,
-  //         description: action.description
-  //     };
-  //
-  //     ctx.setState({
-  //         ...state,
-  //         items: [...state.items, newItem],
-  //     });
-  addBoard(
-    { getState, patchState }: StateContext<BoardStateModel>,
-    { payload }: AddBoard,
-  ) {
-    return this.boardService.addBoard(payload).pipe(
-      tap((result: any) => {
-        const state = getState();
-        patchState({
-          items: [...state.items, result],
-        });
-      }),
-    );
-  }
+    @Action(AddBoard)
+    addBoard(
+        {getState, patchState}: StateContext<BoardStateModel>,
+        {payload}: AddBoard,
+    ) {
+        return this.boardService.addBoard(payload).pipe(
+            tap((result: any) => {
+                const state = getState();
+                patchState({
+                    items: [...state.items, result],
+                });
+            }),
+        );
+    }
 
-  @Action(GetAllBoards)
-  getAllBoards({ getState, patchState }: StateContext<BoardStateModel>) {
-    return this.boardService.getAllBoards().pipe(
-      tap((result: any) => {
-        const state = getState();
-        patchState({
-          items: result,
-        });
-      }),
-    );
-  }
+    @Action(GetAllBoards)
+    getAllBoards({getState, patchState}: StateContext<BoardStateModel>) {
+        return this.boardService.getAllBoards().pipe(
+            tap((result: any) => {
+                const state = getState();
+                patchState({
+                    items: result,
+                });
+            }),
+        );
+    }
 
-  @Action(DeleteBoard)
-  deleteBoard(
-    { getState, setState }: StateContext<BoardStateModel>,
-    { id }: DeleteBoard,
-  ) {
-    return this.boardService.deleteBoard(id).pipe(
-      tap((res: Board) => {
-        const state = getState();
-        const filteredBoards = state.items.filter((board) => board.id !== id);
-        setState({
-          ...state,
-          items: filteredBoards,
-        });
-      }),
-    );
-  }
+    @Action(DeleteBoard)
+    deleteBoard(
+        {getState, setState}: StateContext<BoardStateModel>,
+        {id}: DeleteBoard,
+    ) {
+        return this.boardService.deleteBoard(id).pipe(
+            tap((res: Board) => {
+                const state = getState();
+                const filteredBoards = state.items.filter((board) => board.id !== id);
+                setState({
+                    ...state,
+                    items: filteredBoards,
+                });
+            }),
+        );
+    }
 
-  @Action(UpdateBoard)
-  updateBoard({}: StateContext<BoardStateModel>, action: UpdateBoard) {
-    const formBody = { name: action.name, description: action.description };
-    return this.boardService.updateBoard(action.id, formBody);
-  }
+    @Action(UpdateBoard)
+    updateBoard({}: StateContext<BoardStateModel>, action: UpdateBoard) {
+        const formBody = {name: action.name, description: action.description};
+        return this.boardService.updateBoard(action.id, formBody);
+    }
 }
